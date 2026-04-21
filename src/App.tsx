12652f50627eeba6a5ca2b6ded02b30287604339
Surakshya Khatri <surakshyakhatri08@gmail.com>
2026-04-21 17:55:20 +0545

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import UserForm from "./components/UserForm";
+import { useState } from "react";
+import CountryCard from "./components/country-card";
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <main>
-      <UserForm />
+    <main className="p-10 flex flex-col items-center">
+      <div className="w-full max-w-sm mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900">
+          Type "nepal" to see details:
+        </label>
+        <input
+          type="text"
+          placeholder="Search country..."
+          className="border p-2.5 w-full rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 outline-none"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <CountryCard name={searchTerm} />
     </main>
   );
 }
